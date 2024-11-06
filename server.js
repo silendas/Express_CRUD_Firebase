a892778a9c4d706bf8f43a7727f62065e52e0ad9
silendas <muhammadyasminnulhakim34@gmail.com>
2024-11-06 14:07:06 +0700

    cors

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import studentRoutes from "./routes/studentRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/students", studentRoutes);
